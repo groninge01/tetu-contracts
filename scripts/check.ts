e15e7528ee0d657909eac91e7dbcd46e6b59bacc
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2022-01-12 13:22:20 +0500

    AMB withdraw after setTargetPercentage

--- scripts/check.ts
+++ scripts/check.ts
@@ -2,37 +2,37 @@ import {DeployerUtils} from "./deploy/DeployerUtils";
 import {SmartVault__factory, StrategyAaveMaiBal__factory} from "../typechain";
 import {ethers} from "hardhat";
 import {utils} from "ethers";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 
 async function main() {
   // const signer = await DeployerUtils.impersonate();
   const signer = (await ethers.getSigners())[0];
-  const user = await DeployerUtils.impersonate('0x8f3a89a0b67478b8c78c0b79a50f0454d05c1f1e');
-  const core = await DeployerUtils.getCoreAddressesWrapper(signer);
-  const tools = await DeployerUtils.getToolsAddressesWrapper(signer);
+  // const controller = await DeployerUtils.impersonate('0x6678814c273d5088114B6E40cC49C8DB04F9bC29');
+  // const core = await DeployerUtils.getCoreAddressesWrapper(signer);
+  // const tools = await DeployerUtils.getToolsAddressesWrapper(signer);
 
   // TETU_MATIC_FORK_BLOCK=23116847
-  const vault = await SmartVault__factory.connect('0xf203b855b4303985b3dd3f35a9227828cc8cb009', user);
+  const user = await DeployerUtils.impersonate('0x0ffeb87106910eefc69c1902f411b431ffc424ff');
+  const vault = await SmartVault__factory.connect('0x9C233cA476184e9aa4f2ff07E081D55f179964Fd', user);
   const strategyAddress = await vault.strategy();
-  const strategy = await StrategyAaveMaiBal__factory.connect(strategyAddress, user);
+
+  const amount = ethers.utils.parseUnits('1', 18)
+  console.log('amount', amount.toString());
+  // await vault.withdraw(amount);
   const ppfs = await vault.getPricePerFullShare();
   console.log('ppfs', ppfs.toString());
-  const cp = await strategy.collateralPercentage();
-  console.log('collateralPercentage', cp.toString());
 
-  await vault.depositAndInvest(utils.parseUnits('1'));
+  const controller = await DeployerUtils.impersonate('0x6678814c273d5088114B6E40cC49C8DB04F9bC29');
+  const strategy = await StrategyAaveMaiBal__factory.connect(strategyAddress, controller);
+  await strategy.setTargetPercentage(300);
 
   const ppfsAfter = await vault.getPricePerFullShare();
   console.log('ppfsAfter', ppfsAfter.toString());
-  const cpAfter = await strategy.collateralPercentage();
-  console.log('collateralPercentageAfter', cpAfter.toString());
 
-  const ppfsRatio = ppfsAfter.mul(10000).div(ppfs)
-  console.log('ppfsRatio%', ppfsRatio.toNumber()/100);
-  const cpRatio = cpAfter.mul(10000).div(cp)
-  console.log('cpRatio  %', cpRatio.toNumber()/100)
+  await vault.withdraw(amount);
 
-  const one = cpRatio.mul(ppfsRatio)
-  console.log('one      %', one.toNumber()/1000000);
+  const ppfsAfter2 = await vault.getPricePerFullShare();
+  console.log('ppfsAfter2', ppfsAfter2.toString());
 }
 
 
